aa0ae2c9dadb8a41a545bdcb5b4454a7ea13f28c
atallah-salah <atallah_salah@outlook.com>
2018-02-25 09:30:16 +0200

    stringsQuestions

--- strings.js
+++ strings.js
@@ -2,13 +2,19 @@
 // 	Input :: "a"
 // 	Ouput :: "Position of alphabet: 1"
 	function characPosit(character){
-		//your code is here
+
+		return "Position of alphabet: "+ ((character.toLowerCase().charCodeAt())-96);
 	}
 
 // Write a function called repeatStr which repeats the given
 // string string exactly n times.
 // repeatStr("hello",4) ==> 'hellohellohellohello'
 	
-	function repeatStr (n, s) {
-		//your code is here
+	function repeatStr (s,n) {
+		var result="";
+		for (var index = 0; index < n; index++) {
+			 result +=s;
+			
+		}
+		return result;
 	}
